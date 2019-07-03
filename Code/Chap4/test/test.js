56f1eaee8147d88f8c4d31cec4fb8333258e3d12
alvinwu <conf1102@163.com>
2019-07-03 17:26:37 +0800

    Mocha Test Case Initial

--- Code/Chap4/test/test.js
+++ Code/Chap4/test/test.js
@@ -1,8 +1,20 @@
+function sampleProvinceData() {
+  return {
+    name: "Asia",
+    producers: [
+      { name: "Byzantium", cost: 10, production: 9 },
+      { name: "Attalia", cost: 12, production: 10 },
+      { name: "Sinope", cost: 10, production: 6 },
+    ],
+    demand: 30,
+    price: 20
+  };
+}
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe('provinceTest', function () {
+  it('shortfall', function () {
+    var Province = require('./Province.js');
+    var asia1 = new Province(sampleProvinceData());
+    assert.equal(asia1.shortfall, 5);
   });
 });
