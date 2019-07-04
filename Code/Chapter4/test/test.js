505435ac4fbf96d28bd22d27a58963ca419cf401
alvinwu <conf1102@163.com>
2019-07-04 14:19:07 +0800

    add test case no producers

--- Code/Chapter4/test/test.js
+++ Code/Chapter4/test/test.js
@@ -38,3 +38,22 @@ describe('provinceTest2', function () {
     expect(asia.profit).equal(292);
   });
 });
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: "No proudcers",
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function () {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it('profit', function () {
+    expect(noProducers.profit).equal(0);
+  });
+});
