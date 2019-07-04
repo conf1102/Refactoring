2d36e6461cd941d74851ef390889b39b439bb253
alvinwu <conf1102@163.com>
2019-07-04 15:51:03 +0800

    Extract Function recordDueDate

--- src/Chapter6/ExtractFunctionExample.js
+++ src/Chapter6/ExtractFunctionExample.js
@@ -1,15 +1,11 @@
 function printOwing(invoice) {
     let outstanding = 0;
-
     printBanner();
-
     // calculate outstanding
     for (const o of invoice.orders) {
         outstanding += o.amount;
     }
-    // record due date
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    recordDueDate(invoice);
     printDetails(invoice,outstanding);
 }
 
@@ -23,4 +19,9 @@ function printDetails(invoice,outstanding){
     console.log('name: ${invoice.customer}');
     console.log('amount: ${outstanding}');
     console.log('due: ${invoice.dueDate.toLocaleDateString()}');
+}
+
+function recordDueDate(invoice){
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
