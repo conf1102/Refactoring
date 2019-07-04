8fd729b76cc74d7f0c561337060295a144e0d905
alvinwu <conf1102@163.com>
2019-07-04 15:35:31 +0800

    Extract Function printDetails

--- Code/Chapter6/ExtractFunctionExample.js
+++ Code/Chapter6/ExtractFunctionExample.js
@@ -10,14 +10,17 @@ function printOwing(invoice) {
     // record due date
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-    //print details
-    console.log(`name: ${invoice.customer}`);
-    console.log(`amount: ${outstanding}`);
-    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+    printDetails(invoice,outstanding);
 }
 
 function printBanner(){
     console.log("***********************");
     console.log("**** Customer Owes ****");
     console.log("***********************");
+}
+
+function printDetails(invoice,outstanding){
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
