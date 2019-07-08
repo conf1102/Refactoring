f8f1aa1b59bdedb180bb7cc2680ac9ece1304898
alvinwu <conf1102@163.com>
2019-07-08 14:20:56 +0800

    1.Move distance radians function to top_calculateDistance function. 2.Add points parameter.

--- src/Chapter8/MoveFunction.js
+++ src/Chapter8/MoveFunction.js
@@ -14,21 +14,22 @@ function trackSummary(points) {
         }
         return result;
     }
+    function calculateTime() {
+        // ... 
+    }
+}
+
+function top_calculateDistance(points) {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+        result += distance(points[i - 1], points[i]);
+    }
+    return result;
+
     function distance(p1, p2) {
         // ...
     }
     function radians(degrees) {
         // ... 
     }
-    function calculateTime() {
-        // ... 
-    }
-}
-
-function top_calculateDistance() {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-        result += distance(points[i - 1], points[i]);
-    }
-    return result;
 }
